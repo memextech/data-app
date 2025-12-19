f6b79041cbc5bd46ff1d7c8eaccc306d53e3ef85
chilang <chilang@gmail.com>
2025-12-19 18:58:17 +0000

    update app title

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Data App",
-    layout="wide",
-    initial_sidebar_state="collapsed"
+    page_title="Data App", layout="wide", initial_sidebar_state="collapsed"
 )
 
-st.title("Welcome to Your App")
+st.title("Welcome to Memex")
 st.markdown("Start building something amazing.")
